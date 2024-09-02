0013da922e475a135c8670994c7d06e3acf0dddc
vikram17dp <vikramdp505@gmail.com>
2024-09-02 11:01:10 +0530

    final setup for the header page

--- client/src/compnents/Header.jsx
+++ client/src/compnents/Header.jsx
@@ -1,54 +1,104 @@
-import React from 'react'
-import {Button, Navbar, TextInput} from 'flowbite-react'
-import {Link} from 'react-router-dom'
-import {AiOutlineSearch} from 'react-icons/ai'
-import {FaMoon} from 'react-icons/fa'
+import React, { useState } from "react";
+import { Button, Navbar, TextInput } from "flowbite-react";
+import { Link } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
+import { HiMenu, HiX } from "react-icons/hi";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <Navbar className='border-b-2  '>
-      <Link to='/' className='self-center text-sm sm:text-xl  whitespace-nowrap font-semibold dark:text-white '>
-        <span className='px-3 rounded-lg ml-1 py-1 text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>{`Vikram's`} </span>
+    <Navbar className="border-b-2">
+      <Link
+        to="/"
+        className="self-center text-sm sm:text-xl whitespace-nowrap font-semibold dark:text-white"
+      >
+        <span className="px-3 rounded-lg ml-4 py-1.5 text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+          {`Vikram's`}
+        </span>
         Blog
       </Link>
-      <form className='lg:ml-[-20vw] lg:mb-[2vh] lg:mt-2 '>
-        <TextInput 
-          type='text'
-          placeholder='search...'
-          
-          className='hidden lg:inline w-full  justify-between '
-         
+      <form className="lg:ml-[10vw] lg:mb-[2vh] lg:mt-3">
+        <TextInput
+          type="text"
+          placeholder="search..."
+          className="hidden lg:inline w-full justify-between"
         />
       </form>
-      <Button className='w-12 h-10 rounded-[50%] mr-8 lg:hidden ' color='gray' >
-        <AiOutlineSearch className='self-center mt-3  '/>
-      </Button>
-      <div className='flex gap-2 md:order-2 lg:mr-10'>
-        <Button className='w-12 h-10 hidden  sm:inline items-center rounded-[40%] ' color='gray'>
-          <FaMoon className='self-center '/>
-        </Button>
-        <Link to='/signin'>
-            <Button  className='px-3 py-0.3  bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
-              Sign In
-            </Button>
+      <ul className="hidden lg:flex justify-evenly">
+        <Link to="/">
+          <li className="text-slate-700 mr-5 font-semibold hover:text-blue-400">
+            Home
+          </li>
         </Link>
+        <Link to="/about">
+          <li className="text-slate-700 mr-5 font-semibold hover:text-blue-400">
+            About
+          </li>
+        </Link>
+        <Link to="/projects">
+          <li className="text-slate-700 font-semibold hover:text-blue-400">
+            Projects
+          </li>
+        </Link>
+      </ul>
+      <div className="flex gap-2 md:order-2 lg:mr-6">
+        <Button
+          className="w-12 h-10 hidden sm:inline items-center rounded-[40%]"
+          color="gray"
+        >
+          <FaMoon className="self-center" />
+        </Button>
+        <div className="">
+          <Link to="/signin">
+            <button className="px-4 py-2 rounded-lg border-2 border-pink-300 hover:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+              Sign In
+            </button>
+          </Link>
+        </div>
 
+        <Button
+          className="w-12 h-10 mt-2 border-none lg:hidden"
+          color="gray"
+          onClick={toggleMenu}
+        >
+          {isOpen ? (
+            <HiX className="self-center" />
+          ) : (
+            <HiMenu className="self-center" />
+          )}
+        </Button>
       </div>
-      <Navbar.Collapse>
-        <Navbar.Toggle/>
-     <Navbar.Link>
-      <Link to='/'>Home</Link>
-     </Navbar.Link>
-     <Navbar.Link>
-      <Link to='/projects'>Projects</Link>
-     </Navbar.Link>
-     <Navbar.Link>
-      <Link to='/about'>About</Link>
-     </Navbar.Link>
-      </Navbar.Collapse>
-
+      <div
+        className={`lg:hidden ${
+          isOpen ? "block" : "hidden"
+        } absolute top-16 right-0 bg-white shadow-md rounded-lg p-4`}
+      >
+        <ul className="flex flex-col space-y-2">
+          <Link to="/" onClick={toggleMenu}>
+            <li className="text-slate-700 font-semibold hover:text-blue-400">
+              Home
+            </li>
+          </Link>
+          <Link to="/about" onClick={toggleMenu}>
+            <li className="text-slate-700 font-semibold hover:text-blue-400">
+              About
+            </li>
+          </Link>
+          <Link to="/projects" onClick={toggleMenu}>
+            <li className="text-slate-700 font-semibold hover:text-blue-400">
+              Projects
+            </li>
+          </Link>
+        </ul>
+      </div>
     </Navbar>
-  )
+  );
 }
 
-export default Header
+export default Header;
